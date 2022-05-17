bc5785dadfa22b3e1aeb682c55d88562018738ba
Jeremy <jeremymenchik@dhcp-wifi-8021x-155-41-8-104.bu.edu>
2022-05-17 15:28:59 -0400

    Fixed the problem that kept Hegel from appearing in the plot

--- frequencePlot.py
+++ frequencePlot.py
@@ -11,13 +11,14 @@ import pandas as pd
 HegelFreq=pd.read_csv('hegelFreq.csv')
 MacyFreq=pd.read_csv('macyFreq.csv')
 MontFreq=pd.read_csv('montFreq.csv')
-WeberFreq=pd.read_csv('webFreq.csv')
+WebFreq=pd.read_csv('webFreq.csv')
 WilsonFreq=pd.read_csv('WilsonFreq.csv')
+
 
 plt.rcParams['font.sans-serif'] = "Times New Roman"
 plt.rcParams['font.family'] = "sans-serif"
 plt.locator_params(axis="x", nbins=13)
-plt.locator_params(axis="y", nbins=5)
+plt.locator_params(axis="y", nbins=6)
 plt.xlabel("Year", loc='center')
 plt.ylabel("Frequency of Usage by Author", loc='center')
 plt.xticks(fontsize=10)
@@ -25,22 +26,22 @@ plt.yticks(fontsize=10)
 plt.ylim(top=25)  
 plt.ylim(bottom=0)
 
-HegelFreq['Rolling'] = HegelFreq['Frequence'].rolling(5).mean()
 MacyFreq['Rolling'] = MacyFreq['Frequence'].rolling(5).mean()
 MontFreq['Rolling'] = MontFreq['Frequence'].rolling(5).mean()
-WeberFreq['Rolling'] = WeberFreq['Frequence'].rolling(5).mean()
+WebFreq['Rolling'] = WebFreq['Frequence'].rolling(5).mean()
 WilsonFreq['Rolling'] = WilsonFreq['Frequence'].rolling(5).mean()
+HegelFreq['Rolling'] = HegelFreq['Frequence'].rolling(5).mean()
 
 HegelFreq['Rolling']= 100*HegelFreq['Rolling']
 MacyFreq['Rolling']= 100*MacyFreq['Rolling']
 MontFreq['Rolling']= 100*MontFreq['Rolling']
-WeberFreq['Rolling']= 100*WeberFreq['Rolling']
+WebFreq['Rolling']= 100*WebFreq['Rolling']
 WilsonFreq['Rolling']= 100*WilsonFreq['Rolling']
 
-plt.plot(HegelFreq['Hegel'], HegelFreq['Rolling'], label = "Hegel", color="gainsboro",linewidth=1, linestyle="dashed")
+plt.plot(HegelFreq['Year'], HegelFreq['Rolling'], label = "Hegel", color="gainsboro",linewidth=1, linestyle="dashed")
 plt.plot(MacyFreq['Year'], MacyFreq['Rolling'], label = "Macy", color="darkgrey",linewidth=1, linestyle="dotted")
 plt.plot(MontFreq['Year'], MontFreq['Rolling'], label = "Montesquieu", color="dimgrey",linewidth=1, linestyle="dashdot")
-plt.plot(WeberFreq['Year'], WeberFreq['Rolling'], label = "Weber", color="black",linewidth=1, linestyle="solid")
+plt.plot(WebFreq['Year'], WebFreq['Rolling'], label = "Weber", color="black",linewidth=1, linestyle="solid")
 plt.plot(WilsonFreq['Year'], WilsonFreq['Rolling'], label = "Wilson", color="lightgrey",linewidth=1, linestyle="solid")
 
 
